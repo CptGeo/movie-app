cecd41107d8271fadf2272120ae949f1b790ec34
cptgeo <georgekalyvianakis18@gmail.com>
2025-08-31 21:58:05 +0200

    Hide statusbar in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,13 @@
 import { Stack } from "expo-router";
+import {StatusBar} from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-  </Stack>;
+  return <>
+      <StatusBar hidden />
+      <Stack>
+
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+  </>;
 }
