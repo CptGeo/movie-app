518f6409177b92e49d1fbd6d0a2e3e74063396c7
cptgeo <georgekalyvianakis18@gmail.com>
2025-08-31 21:59:45 +0200

    Implement trending movies section in index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,23 +7,53 @@ import { useFetch } from "@/services/useFetch";
 import { useRouter } from "expo-router";
 import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 import "../globals.css";
+import {getTrendingMovies} from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard/TrendingCard";
 
 export default function Index() {
   const router = useRouter();
-  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: '' }));
+  const { data: trendingMovies, loading: trendingLoading, error: trendingError } = useFetch(() => getTrendingMovies());
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: ''}));
+
+  const moviesMap: Record<string, TrendingMovie> = {};
+  trendingMovies?.forEach(movie => {
+      if(!moviesMap[movie.movie_id]) {
+          moviesMap[movie.movie_id] = movie;
+      }
+  });
+
+  const filteredMovies = Object.values(moviesMap).sort((a, b) => a.count - b.count);
+
 
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center" />)
-          : moviesError ? (
-            <Text>Error: {moviesError.message}</Text>
+          : moviesError || trendingError ? (
+            <Text>Error: {moviesError?.message ?? trendingError?.message}</Text>
           ) : (
             <View className="flex-1 mt-5">
-              <SearchBar onPress={() => router.push("/search")} placeholder="Search for a movie..." />
+              <SearchBar onPress={() => router.push("/search")} placeholder="Search for a movie..." value="" />
+                {filteredMovies && (
+                    <View className="mt-10">
+                        <Text className="text-lg text-white font-bold mb-3">Trending Movies</Text>
+                        <FlatList
+                            className="mb-4 mt-3"
+                            data={filteredMovies}
+                            horizontal
+                            pagingEnabled
+                            snapToAlignment="start"
+                            showsHorizontalScrollIndicator={false}
+                            renderItem={({ item, index }) => (
+                                <TrendingCard movie={item} index={index} />)}
+                            ItemSeparatorComponent={() => <View className="w-8" />}
+                            keyExtractor={(item) => item.movie_id.toString()}
+                        />
+                    </View>
+                )}
                 <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
                 <FlatList 
                   data={movies}
