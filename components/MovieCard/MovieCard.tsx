c1ff16338f68cc4e4db83dcca0eaae4adcae1dd1
cptgeo <georgekalyvianakis18@gmail.com>
2025-09-02 22:30:50 +0200

    Fix MovieCard link path and reorder imports for consistency

--- components/MovieCard/MovieCard.tsx
+++ components/MovieCard/MovieCard.tsx
@@ -1,13 +1,13 @@
+import { icons } from "@/constants/icons";
 import { Href, Link } from "expo-router";
-import {Image, Text, TouchableOpacity, View} from "react-native";
-import {icons} from "@/constants/icons";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 
 
 const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie ) => {
 
     return (
-        <Link href={`/movie/${id}` as Href} asChild>
+        <Link href={`/movies/${id}` as Href} asChild>
             <TouchableOpacity className="w-[30%]">
                 <Image 
                 source={{
